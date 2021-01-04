b8e6ebf3e8107785ea108f80c03bb3dcb086d0e3
Prahlad Yeri <prahladyeri@yahoo.com>
2021-01-04 16:53:39 +0530

    chore: updates

--- app.js
+++ app.js
@@ -4,37 +4,9 @@ function readTextContent(data) {
 	return data.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/);
 }
 
-window.addEventListener("DOMContentLoaded", function(){
-	console.log("All is well!");
-	var urlParams = new URLSearchParams(window.location.search);
-	var ct = urlParams.get('ct'); //?ct=au
-	var state = urlParams.get('state');
-	var city = urlParams.get('city');
-	var prof = urlParams.get('prof');
-	
-	if (ct == null) { //list all countries
-		$.get(SITEMAP_INDEX_URL, function(data){
-			data = readTextContent(data);
-			parser = new DOMParser();
-			xmlDoc = parser.parseFromString(data,"text/xml");
-			console.log('xml data:', xmlDoc);
-		});
-		return;
-	}
-	else if (state==null || city==null || prof==null) {
-		//list all details for this country
-		return;
-	}
-	
-	var title = prof + " in " + city + ", " + state;
-	document.title = title + " - " + document.title;
-	$("#lblTitle").text(title);
-	
+function showListings(ct, state, city, prof) {
 	var url = "/data/"+ct+"/"+state+"/"+city+"/"+prof+".json";
 	$.getJSON(url, function(data){
-		//console.log("received..");
-		//window.ddata = data;
-		//console.log(typeof data,data);
 		data.data.forEach(function(item, idx){
 			//console.log("item:", item, "idx:", idx);
 			html = "<tr>";
@@ -55,6 +27,47 @@ window.addEventListener("DOMContentLoaded", function(){
 			$("#tblMain").append(html);
 		});
 	});
+}
+
+window.addEventListener("DOMContentLoaded", function(){
+	console.log("All is well!");
+	var urlParams = new URLSearchParams(window.location.search);
+	var ct = urlParams.get('ct'); //?ct=au
+	var state = urlParams.get('state');
+	var city = urlParams.get('city');
+	var prof = urlParams.get('prof');
+	
+	if (ct == null) { //list all countries
+		$.get(SITEMAP_INDEX_URL, function(xmldata){
+			//window.xmldata = data;
+			//console.log('xml data:', data);
+			 
+			locs = xmldata.getElementsByTagName('sitemapindex')[0].getElementsByTagName('sitemap');
+			for(var i=0;i<locs.length;i++){
+				var item = locs[i];
+				url = new URL(item.getElementsByTagName('loc')[0].textContent);
+				parts = url.pathname.split('/');
+				ct = parts[parts.length-1].split('.txt')[0];
+				//$("#lblTitle").text(ct  + " - " + document.title);
+				$("#articleBlock").append("<a href='/?ct="+ct+"'><h1>"+ct+"</h1></a>");
+				$("#articleBlock").append("<br>");
+			};
+			//parser = new DOMParser();
+			//xmlDoc = parser.parseFromString(data,"text/xml");
+			//console.log('xml data:', xmlDoc);
+		});
+		return;
+	}
+	else if (state==null || city==null || prof==null) {
+		//list all details for this country (see below the if block)
+		//@todo: make the footer block bigger as there will be many entries in this case
+	}
+	else { //all entries proper
+		var title = prof + " in " + city + ", " + state;
+		document.title = title + " - " + document.title;
+		$("#lblTitle").text(title);
+		showListings(ct, state, city, prof);
+	}
 	
 	
 	//console.log('url:', url);
